5ea935673eb847e3a0c071eded5cb92633787099
Kenny Lam <kinwai.lam730@gmail.com>
2020-01-25 22:24:12 -0500

    Added the question pool as basic variables for test

---
+++ script.js
@@ -0,0 +1,37 @@
+var startBut = document.querySelector("#start")
+
+var codingPool = {
+  "codingPool": [
+    {
+      "question": "Question 1 here",
+      "sel": [
+        "answer 1",
+        "answer 2",
+        "answer 3",
+        "answer 4"
+      ],
+      "answer": "answer 1"
+    },
+    {
+      "question": "Question 2 here",
+      "sel": [
+        "answer 1",
+        "answer 2",
+        "answer 3",
+        "answer 4"
+        ],
+      "answer": "answer 2"
+    },
+    {
+      "question": "Question 3 here",
+      "sel": [
+        "answer 1",
+        "answer 2",
+        "answer 3",
+        "answer 4"
+        ],
+      "answer": "answer 3"
+      }
+    
+  ]
+}
